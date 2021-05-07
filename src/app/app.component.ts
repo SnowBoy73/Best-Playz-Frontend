8ef3d635d254866fef90f5a80831e4f17aa6e1b1
SnowBoy73 <56825266+SnowBoy73@users.noreply.github.com>
2021-05-07 14:31:49 +0200

    Rebuild after Git crash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import {CommentService} from './comment/shared/comment.service';
 import {StorageService} from './shared/storage.service';
 import {CommentComponent} from './comment/comment.component';
 import {ClientModel} from './comment/shared/client.model';
+import {FormControl} from '@angular/forms';
+import {Socket} from 'ngx-socket-io';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +18,12 @@ export class AppComponent {
   userNickname: string | undefined;
   loggedInUser: ClientModel | undefined;
   loginRequest: boolean | undefined;
-  constructor(private storageService: StorageService) { }
+  loginFC = new FormControl('');
+
+  constructor(
+    private storageService: StorageService,
+    private socket: Socket,
+  ) { }
 
   /* Can put global error listening here (from comment service)!!
   listenForErrors(): Observable<string> {
@@ -29,7 +36,25 @@ export class AppComponent {
   login(): void {
 
   }
+/*
+login(): void {
+    // this.loginRequest = true;
+    location.reload();
 
+    /*if (this.loginFC.value) {
+      const dto: loginDto = {nickname: this.loginFC.value};
+      this.commentService.sendLogin(dto);
+    }
+}
+
+sendLogin(dto: loginDto): void {
+  // this.loginRequest = false;
+
+  console.log(dto.nickname);
+  this.socket.emit('login', dto);
+}
+
+ */
     logout(): void {
     console.log('Logout called in App Comp');
       // localStorage.clear();
@@ -39,11 +64,14 @@ export class AppComponent {
     if (this.loggedInUser !== undefined) {
       console.log('logout id is DEFINED as:', this.storageService.loadClient()?.id);
 
+      this.socket.emit('logout', this.loggedInUser.id);
 
 
       //this.storageService.deleteClient(this.loggedInUser.id);
       localStorage.clear();
       console.log('local storage cleared:', this.storageService.loadClient()?.id);
+
+      location.reload();
 
     } else {
       console.log('logout id == undefined:', this.storageService.loadClient()?.nickname);
